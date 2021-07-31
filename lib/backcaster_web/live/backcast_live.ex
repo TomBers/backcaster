f08d0e4c4420f7f86583cc785a182308cc9febf8
TomBers <tjcberman@gmail.com>
2021-08-01 00:07:22 +0100

    Close to MVP

--- lib/backcaster_web/live/backcast_live.ex
+++ lib/backcaster_web/live/backcast_live.ex
@@ -1,10 +1,15 @@
 defmodule Section do
   use Surface.LiveComponent
+  alias Surface.Components.Form
+  alias Surface.Components.Form.{TextInput, Label, Field}
 
   prop title, :string
   prop value, :string
 
-  prop click, :event, required: true
+  data vals, :map, default: %{"name" => "", "email" => ""}
+
+  data edit, :boolean, default: false
+  prop submit, :event, required: true
 
   def render(assigns) do
     ~F"""
@@ -12,10 +17,31 @@ defmodule Section do
       <div class="card-body">
         <h2 class="card-title">{@title}</h2>
         <p>{@value}</p>
-        <button class="button is-info" :on-click={@click} phx-value-title={@title}>+</button>
+        <button class="button is-info" :on-click="edit">Edit</button>
+        <div :if={@edit}>
+          <Form for={:vals} submit={@submit} opts={autocomplete: "off"}>
+          <Field class="field" name="new value">
+            <Label class="label"/>
+            <div class="control">
+              <TextInput class="input" value={@value}/>
+            </div>
+          </Field>
+          <Field class="field" name="title">
+            <div class="control">
+              <TextInput class="hidden" value={@title}/>
+            </div>
+          </Field>
+          <input type="submit" value="Submit" :on-click="edit">
+        </Form>
+
+        </div>
       </div>
     </div>
     """
+  end
+
+  def handle_event("edit", _, socket) do
+    {:noreply, update(socket, :edit, fn _ -> !socket.assigns.edit end)}
   end
 
 end
@@ -32,8 +58,8 @@ defmodule BackcasterWeb.BackcastLive do
     {:ok, socket}
   end
 
-  def handle_event("update_field", %{"title" => title}, socket) do
-    new_backcast = Map.replace(socket.assigns.backcast, title, "BOB")
+  def handle_event("update_field", %{"vals" => %{"new value" => new_val, "title" => title}}, socket) do
+    new_backcast = Map.replace(socket.assigns.backcast, title, new_val)
     {:noreply, assign(socket, :backcast, new_backcast)}
   end
 
