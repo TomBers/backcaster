81616094e22f1dab12338f06f529875d39857938
TomBers <tjcberman@gmail.com>
2021-08-03 10:08:47 +0100

    Clean up tests

--- cypress/integration/test_section.js
+++ cypress/integration/test_section.js
@@ -7,15 +7,12 @@ describe('Create and Edit a section', () => {
       cy.visit('http://localhost:4000/backcast/' + test)
     })
 
-
-
      it('can create a section', () => {
         cy.get('#add-section-btn').click({force: true})
         cy.wait(wait)
         cy.get(".create-section-btn").first().click({force: true})
         cy.wait(wait)
      })
-
 
      it('can edit a section', () => {
          let edit_button = cy.get(".edit-section").first().click({force: true});
@@ -26,28 +23,3 @@ describe('Create and Edit a section', () => {
         })
 
  })
-
- describe('Create and Edit a milestone', () => {
- beforeEach(() => {
-       cy.visit('http://localhost:4000/backcast/' + test)
-     })
-
-    it('can create a milestone', () => {
-        cy.get('.add-milestone').click({force: true})
-        cy.wait(wait)
-        cy.get('#vals_title').type("A new Milestone")
-        cy.get('#vals_date').clear().type("2021-11-17")
-        cy.get('.milestone-submit').click({force: true})
-        cy.wait(wait)
-    })
-
-    it('can edit a milestone', () => {
-     let edit_button = cy.get(".edit-milestone").first().click({force: true});
-     cy.wait(wait)
-     cy.get('#vals_title').clear().type("Updated milestone")
-     cy.get('#vals_date').clear().type("2021-11-17")
-     cy.get('.milestone-submit').click({force: true})
-     cy.wait(wait)
-    })
-
-})
