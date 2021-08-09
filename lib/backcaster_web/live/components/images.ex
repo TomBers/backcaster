dd271e1b3e1ecb5808c935bb83eccf72e35c8409
TomBers <tjcberman@gmail.com>
2021-08-09 10:05:29 +0100

    make image upload a comp

--- lib/backcaster_web/live/components/images.ex
+++ lib/backcaster_web/live/components/images.ex
@@ -1,10 +1,21 @@
 defmodule Images do
   use Surface.LiveComponent
+  prop images, :map
+  prop parent_pid, :string
+  prop store_image, :event, required: true
 
   def render(assigns) do
     ~F"""
         <div class="card shadow-lg md:card-side bg-secondary">
           <div class="card-body">
+    {#if length(Map.keys(@images)) == 0 }
+      <ImageUpload store_image={@store_image} parent_pid={@parent_pid} id="imageUploads" />
+    {#else}
+        {#for {id, image} <- @images}
+          <img src={image["path"]} />
+      {/for}
+    {/if}
+
   </div>
     </div>
 
