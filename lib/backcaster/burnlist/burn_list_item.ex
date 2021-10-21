ae58d182d93d79faba080ee167c8b4fee5116e87
TomBers <tjcberman@gmail.com>
2021-10-21 11:48:34 +0100

    make pretty

--- lib/backcaster/burnlist/burn_list_item.ex
+++ lib/backcaster/burnlist/burn_list_item.ex
@@ -2,10 +2,6 @@ defmodule BurnListItem do
   @enforce_keys [:text]
   defstruct [text: "", state: :active, uuid: nil, created_at: nil, updated_at: nil, category: "default"]
 
-#  def make_item(text) do
-#    %BurnListItem{text: text, uuid: UUID.uuid4(), created_at: Date.add(Date.utc_today(), Enum.random([-1,-2,-3,-4]))}
-#  end
-
   def make_item(text, %BurnListCategory{} = category) do
     created_at = Date.add(Date.utc_today(), Enum.random([-1,-2,-3,-4, -8, -9]))
     %BurnListItem{text: text, uuid: UUID.uuid4(), category: category, created_at: created_at, updated_at: created_at }
--- lib/backcaster_web/live/components/burn_list_add.ex
+++ lib/backcaster_web/live/components/burn_list_add.ex
@@ -17,12 +17,12 @@ defmodule BurnListAdd do
     <span class="emphasis">
       <Form for={:add_item} submit={@add_item_event} opts={autocomplete: "off"}>
         <Field class="field" name="content">
-          <TextArea class="textarea h-24" id={@id} rows="4" opts={placeholder: "(Each line is a new item)"} />
+          <TextArea class="textarea textarea-primary h-16 w-full my-2" id={@id} rows="4" opts={placeholder: "(Each line is a new item)"} />
         </Field>
         <Field class="field" name="category">
           <HiddenInput value={@category.uuid} id={@id} />
         </Field>
-        <input class="btn milestone-submit" type="submit" value="Submit">
+        <input class="btn milestone-submit" type="submit" value="Add items">
       </Form>
     </span>
     """
