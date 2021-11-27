e83b87889eb23ea522e43b7d48ee42237a298ce6
TomBers <tjcberman@gmail.com>
2021-11-27 16:51:13 +0000

    fixed swot template

--- lib/backcaster_web/live/components/templates/swot.ex
+++ lib/backcaster_web/live/components/templates/swot.ex
@@ -7,11 +7,31 @@ defmodule Swot do
 #  TODO - make the boxes have a title (use the card comp??)
   def render(assigns) do
     ~F"""
-    <div class="swot-box-container">
-      <div class="swot-box"><div class="swot-title">Strengths:</div> <InlineEdit backcast={@backcast} category="Strengths" parent_pid={@parent_pid} use_text_area={true} id="strengths" /></div>
-      <div class="swot-box"><div class="swot-title">Weaknesses:</div> <InlineEdit backcast={@backcast} category="Weaknesses" parent_pid={@parent_pid} use_text_area={true} id="weaknesses" /></div>
-      <div class="swot-box"><div class="swot-title">Opportunities:</div> <InlineEdit backcast={@backcast} category="Opportunities" parent_pid={@parent_pid} use_text_area={true} id="opportunities" /></div>
-      <div class="swot-box"><div class="swot-title">Threats:</div> <InlineEdit backcast={@backcast} category="Threats" parent_pid={@parent_pid} use_text_area={true} id="threats" /></div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+      <div class="card">
+          <div class="card-title">Strengths:</div>
+          <div class="card-body">
+            <InlineEdit backcast={@backcast} category="Strengths" parent_pid={@parent_pid} use_text_area={true} id="strengths" />
+          </div>
+      </div>
+      <div class="card">
+          <div class="card-title">Weaknesses:</div>
+          <div class="card-body">
+            <InlineEdit backcast={@backcast} category="Weaknesses" parent_pid={@parent_pid} use_text_area={true} id="weaknesses" />
+          </div>
+      </div>
+      <div class="card">
+          <div class="card-title">Opportunities:</div>
+          <div class="card-body">
+            <InlineEdit backcast={@backcast} category="Opportunities" parent_pid={@parent_pid} use_text_area={true} id="opportunities" />
+          </div>
+      </div>
+      <div class="card">
+          <div class="card-title">Threats:</div>
+          <div class="card-body">
+            <InlineEdit backcast={@backcast} category="Threats" parent_pid={@parent_pid} use_text_area={true} id="threats" />
+          </div>
+      </div>
     </div>
     """
     end
