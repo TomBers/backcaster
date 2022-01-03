101ab44a4e6c986ba76439dc7625314f9d416a3b
TomBers <tjcberman@gmail.com>
2022-01-03 10:57:30 +0000

    updated timeline

--- lib/backcaster_web/live/components/gantt_chart.ex
+++ lib/backcaster_web/live/components/gantt_chart.ex
@@ -1,29 +1,33 @@
 defmodule GanttChart do
   use Surface.LiveComponent
 
-  prop goal_date, :date
-  prop milestones, :map
+  prop board, :map
 
   def render(assigns) do
     ~F"""
     <label for="my-modal-2" class="btn btn-primary modal-button">open timeline</label>
     <input type="checkbox" id="my-modal-2" class="modal-toggle">
     <div class="modal">
-      <div class="modal-box" style="min-width: 90%">
-        <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js" />
-        <script>mermaid.initialize({ startOnLoad: true });</script>
+      <div class="modal-box bg-neutral-content" style="min-width: 90%">
         <div class="mermaid">
           gantt
-            title Project timeline
+            title Timeline
             dateFormat  YYYY-MM-DD
             section Overall
-            GOAL :a, {Date.utc_today()}, {Date.diff(@goal_date, Date.utc_today())}d
+            GOAL :a, {Date.utc_today()}, {Date.diff(@board.goal_date, Date.utc_today())}d
             section Milestones
             . :a1, {Date.utc_today()}, 0d
-            {#for {_id, milestone} <- @milestones}
-              {milestone["title"]} {get_milestone_label(milestone)}, {Date.utc_today()}, {find_milestone_diff(milestone["date"])}d
+            {#for {_id, milestone} <- @board.content["milestones"]}
+              {#if milestone["active"]}
+                {milestone["title"]} :milestone, {milestone["date"]}, 0d
+              {#else}
+                {milestone["title"]} :done, {Date.utc_today()}, {find_milestone_diff(milestone["date"])}d
+              {/if}
+
             {/for}
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js" />
+        <script>mermaid.initialize({ startOnLoad: true });</script>
         <div class="modal-action">
           <label for="my-modal-2" class="btn">Close</label>
         </div>
@@ -37,14 +41,5 @@ defmodule GanttChart do
     |> Date.from_iso8601!()
     |> Date.diff(Date.utc_today())
   end
-
-  def get_milestone_label(milestone) do
-    if milestone["active"] do
-      ":milestone"
-      else
-      ":done"
-    end
-  end
-
 
 end
