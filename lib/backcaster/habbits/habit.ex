75ef815f6a2162a212b93bd0641720a6da9b04f4
TomBers <tjcberman@gmail.com>
2022-01-06 22:01:01 +0000

    first blush at habit interface

--- lib/backcaster/habbits/habit.ex
+++ lib/backcaster/habbits/habit.ex
@@ -1,29 +1,54 @@
 defmodule Habit do
   @enforce_keys [:name, :updated_at]
-  defstruct [:name, :updated_at, update_freq: :daily]
+  defstruct [:name, :updated_at, :uuid, update_freq: :daily]
 
-  def get_habits do
-    [
-      gen_rand(),
-      gen_rand(),
-      gen_rand
-    ]
+  def gen_habits do
+    %{
+      "1" => gen_rand("one"),
+      "2" => gen_rand("two"),
+      "3" => gen_rand("three")
+    }
   end
 
-  def gen_rand do
-    %Habit{name: "a habbit", updated_at: Date.add(Date.utc_today(), Enum.random([-3,-7,-400])), update_freq: Enum.random([:daily, :weekly ,:monthly])}
+  def get_visible_habits(habits) do
+    habits |> Enum.filter(fn {_id, habit} -> Habit.is_due(habit) end) |> Map.new()
   end
 
-  def is_due(%Habit{name: _name, update_freq: :daily, updated_at: updated_at}) do
+  def gen_rand(name) do
+    %Habit{name: name, updated_at: Date.add(Date.utc_today(), Enum.random([-3,-7,-400])), update_freq: Enum.random([:daily, :weekly ,:monthly]), uuid: UUID.uuid4() }
+  end
+
+  def is_due(%Habit{update_freq: :daily, updated_at: updated_at}) do
     Date.diff(Date.utc_today(), updated_at) >= 1
   end
 
-  def is_due(%Habit{name: _name, update_freq: :weekly, updated_at: updated_at}) do
+  def is_due(%Habit{update_freq: :weekly, updated_at: updated_at}) do
     Date.diff(Date.utc_today(), updated_at) >= 7
   end
 
-  def is_due(%Habit{name: _name, update_freq: :monthly, updated_at: updated_at}) do
+  def is_due(%Habit{update_freq: :monthly, updated_at: updated_at}) do
     Date.diff(Date.utc_today(), updated_at) >= 31
+  end
+
+  def complete_habit(habits, id) do
+    update_in(habits[id], fn old -> %Habit{name: "complete", update_freq: old.update_freq, uuid: old.uuid, updated_at: Date.utc_today() } end)
+  end
+
+  def add_new_habit(habits, title, freq) do
+    key = length(Map.keys(habits)) + 1
+    Map.put(habits, "#{key}", new_habit(title, freq))
+  end
+
+  def new_habit(name, "daily") do
+    %Habit{name: name, updated_at: Date.add(Date.utc_today(), -1), update_freq: :daily, uuid: UUID.uuid4() }
+  end
+
+  def new_habit(name, "weekly") do
+    %Habit{name: name, updated_at: Date.add(Date.utc_today(), -7), update_freq: :weekly, uuid: UUID.uuid4() }
+  end
+
+  def new_habit(name, "monthly") do
+    %Habit{name: name, updated_at: Date.add(Date.utc_today(), -31), update_freq: :monthly, uuid: UUID.uuid4() }
   end
 
 end
--- lib/backcaster_web/live/components/habits.ex
+++ lib/backcaster_web/live/components/habits.ex
@@ -1,16 +1,47 @@
 defmodule Habits do
   use Surface.LiveComponent
+  alias Surface.Components.Form
+  alias Surface.Components.Form.{TextInput, Select, Label, Field}
+
+  data is_open, :boolean, default: true
+  data show_form, :boolean, default: true
+  data vals, :map, default: %{"name" => "", "freq" => ""}
 
   def mount(socket) do
 
     visible_habits =
-      Habit.get_habits()
-      |> Enum.filter(fn habit -> Habit.is_due(habit) end)
+      Habit.gen_habits()
+      |> Habit.get_visible_habits()
 
     socket =
       socket
       |> assign(:habits, visible_habits)
     {:ok, socket}
   end
+
+  def handle_event("open_habits", _, socket) do
+    {:noreply, update(socket, :is_open, fn _ -> !socket.assigns.is_open end)}
+  end
+
+  def handle_event("complete_habit", %{"habbit-id" => id}, socket) do
+    new_habits =
+      socket.assigns.habits
+      |> Habit.complete_habit(id)
+      |> Habit.get_visible_habits()
+    {:noreply, update(socket, :habits, fn _ -> new_habits end)}
+  end
+
+  def handle_event("add_new_habit", _, socket) do
+    {:noreply, update(socket, :show_form, fn _ -> !socket.assigns.show_form end)}
+  end
+
+  def handle_event("submit_new_habit", %{"vals" => %{"freq" => freq, "title" => title}}, socket) do
+    socket =
+      socket
+      |> update(:show_form, fn _ -> false end)
+      |> update(:habits, fn old_habits -> Habit.add_new_habit(old_habits, title, freq) end)
+    {:noreply, socket}
+  end
+
 
 end
