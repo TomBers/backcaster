1d697e74d96368372c940fd9278f08cd3965eb4e
TomBers <tjcberman@gmail.com>
2022-01-25 12:49:49 +0000

    looking good

--- lib/backcaster_web/live/components/ClosedMilestones.ex
+++ lib/backcaster_web/live/components/ClosedMilestones.ex
@@ -7,19 +7,16 @@ defmodule ClosedMilestones do
 
   def render(assigns) do
     ~F"""
-    <h1 class="ml-8" :on-click="is_open">Closed ({length(@milestones)})</h1>
+    <h1 class="card-title ml-8 mt-4" :on-click="is_open">Closed ({length(@milestones)})</h1>
     {#if @is_open}
-      <div class="grid grid-cols-1 gap-4 lg:p-4 xl:grid-cols-2 lg:bg-base-200 rounded-box">
+      <div class="grid grid-cols-1 lg:p-4 lg:grid-cols-1  rounded-box">
         {#for {id, milestone} <- @milestones}
-          <div class="card-body">
-            <div class="justify-end card-actions">
-              <h2 class="card-title faded break-all">{milestone["title"]}</h2>
-              <div data-tip="Reopen" class="tooltip tooltip-bottom">
-                <input type="checkbox" :on-click={@change_active} phx-value-id={id} class="toggle toggle-secondary">
-              </div>
-            </div>
-
+          <div class="grid justify-items-stretch grid-cols-3">
+            <h2 class="card-title faded break-all">{milestone["title"]}</h2>
             <p class="faded">Done: {date_completed(milestone["completed"])}</p>
+            <div data-tip="Reopen" class="tooltip tooltip-left justify-self-end">
+                <input type="checkbox" :on-click={@change_active} phx-value-id={id} class="toggle toggle-secondary justify-self-end">
+              </div>
           </div>
         {/for}
       </div>
