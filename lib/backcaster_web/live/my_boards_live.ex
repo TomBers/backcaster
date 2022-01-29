d2a1c0c5749a93cfa48814b246b2cdcc2f59283a
TomBers <tjcberman@gmail.com>
2022-01-29 20:39:59 +0000

    add create new board button

--- lib/backcaster_web/live/my_boards_live.ex
+++ lib/backcaster_web/live/my_boards_live.ex
@@ -1,5 +1,6 @@
 defmodule BackcasterWeb.MyBoardsLive do
   use Surface.LiveView
+  alias Surface.Components.Form
 
   def mount(params, _session, socket) do
     theme = Map.get(params, "theme", "lofi")
