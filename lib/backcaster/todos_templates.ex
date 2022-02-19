5c56711a7aced12a76dc2a7bd3376b2e49c2c015
TomBers <tjcberman@gmail.com>
2022-02-19 10:15:19 +0000

    alpha version

--- lib/backcaster/todos_templates.ex
+++ lib/backcaster/todos_templates.ex
@@ -1,29 +1,25 @@
 defmodule Backcaster.TodosTemplates do
 
   def milestone_templates() do
-    ["Validation", "Marketing"]
+    ["Pick a template", "Validation", "Marketing"]
   end
 
   def gen_template(opt) do
     case opt do
-      "Validation" -> val_template()
+      "Validation" -> gen_items(["Validate", "Ask 1 person", "Ask bob"])
       "Marketing" -> gen_items(["Marketing", "Twitter"])
       _ -> gen_items([])
     end
   end
 
-  def val_template do
-    gen_items(["Validate", "Ask 1 person", "Ask bob"])
-  end
-
-  def gen_items(items) do
+  defp gen_items(items) do
     todo = BurnListCategory.new_category("TODO")
     items
     |> Enum.map(fn item -> BurnListItem.make_item(item, todo) end)
     |> base_template(todo)
   end
 
-  def base_template(items, todo) do
+  defp base_template(items, todo) do
     doing = BurnListCategory.new_category("Doing")
     done = BurnListCategory.new_category("Done")
     board = %BurnListBoard{
--- lib/backcaster_web/live/components/milestone_form.ex
+++ lib/backcaster_web/live/components/milestone_form.ex
@@ -11,6 +11,7 @@ defmodule MilestoneForm do
 
   prop submit, :event, required: true
   prop edit, :event, required: true
+  prop show_template, :boolean, default: false
 
   def render(assigns) do
     ~F"""
@@ -22,13 +23,17 @@ defmodule MilestoneForm do
             <TextInput class="input input-bordered" value={@title} opts={placeholder: "Title"} />
           </div>
         </Field>
+      {#if @show_template}
+        <div class="divider">OR</div>
         <Field class="field" name="template">
           <Select
+            selected="Pick a template"
             field="template"
             options={make_options()}
             class="select select-lg"
           />
         </Field>
+      {/if}
 
         <Field name="id">
           <TextInput class="hidden" value={@id} />
