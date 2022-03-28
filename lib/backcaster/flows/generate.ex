c72c21dade1f3001efe799afa7937d98f2fbca52
TomBers <tjcberman@gmail.com>
2022-03-28 16:54:20 +0100

    created dynamic FSM module

--- lib/backcaster/flows/generate.ex
+++ lib/backcaster/flows/generate.ex
@@ -1,25 +1,41 @@
 defmodule Generate do
 
   def run do
-    mods = [%{
-      module_name: :hello,
-      states: ["A", "B", "C"],
-      transitions: %{
-        "A" => ["B", "C"],
-      },
-      state_qns: %{
-        "A" => %{
-          "Yes" => "B",
-          "No" => "C"
+    mods = [
+      gen_params(
+        :hello,
+        ["A", "B", "C"],
+        %{
+          "A" => ["B", "C"],
+        },
+        %{
+          "A" => %{
+            "Yes" => "B",
+            "No" => "C"
+          }
         }
-      }
-    }]
+      ),
+      gen_params(
+        :world,
+        ["A", "B", "C"],
+        %{
+          "A" => ["B", "C"],
+        },
+        %{
+          "A" => %{
+            "Bob" => "B",
+            "No" => "C"
+          }
+        }
+      )
+    ]
 
     mods
     |> Enum.map(fn params -> Module.create(params.module_name, mod_contents(params), Macro.Env.location(__ENV__)) end)
 
 
-    :hello.get_state_options(%{state: "A"})
+    :hello.get_state_options(%{state: "A"}) |> IO.inspect
+    :world.get_state_options(%{state: "A"}) |> IO.inspect
 
   end
 
@@ -27,44 +43,56 @@ defmodule Generate do
     quote do
       use Machinery,
           states: unquote(states),
-          transitions: %{}
+          transitions: unquote(transitions)
 
-      @state_qns %{}
+      @state_qns unquote(state_qns)
 
       def get_state_options(%{state: state}) do
-        @state_qns |> Map.get(state) |> Map.keys() |> Enum.reverse
+        @state_qns
+        |> Map.get(state)
+        |> Map.keys()
+        |> Enum.reverse
       end
     end
   end
 
+  def gen_params(name, states, transitions, state_qns) do
+    %{
+      module_name: name,
+      states: states,
+      transitions: Macro.escape(transitions),
+      state_qns: Macro.escape(state_qns)
+    }
+  end
 
 
-#  def run do
-#    mods = [:hello, :world]
-#
-#    contents =
-#      quote do
-#        def world, do: true
-#      end
-#
-#    mods
-#    |> Enum.map(fn nme -> Module.create(nme, contents, Macro.Env.location(__ENV__)) end)
-#
-#
-#    IO.inspect(:hello.world())
-#    IO.inspect(:world.world())
-#  end
+
+  #  def run do
+  #    mods = [:hello, :world]
+  #
+  #    contents =
+  #      quote do
+  #        def world, do: true
+  #      end
+  #
+  #    mods
+  #    |> Enum.map(fn nme -> Module.create(nme, contents, Macro.Env.location(__ENV__)) end)
+  #
+  #
+  #    IO.inspect(:hello.world())
+  #    IO.inspect(:world.world())
+  #  end
 
 
-#  def run do
-#    contents =
-#      quote do
-#        def world, do: true
-#      end
-#
-#    Module.create(:hello, contents, Macro.Env.location(__ENV__))
-#
-#    :hello.world()
-#  end
+  #  def run do
+  #    contents =
+  #      quote do
+  #        def world, do: true
+  #      end
+  #
+  #    Module.create(:hello, contents, Macro.Env.location(__ENV__))
+  #
+  #    :hello.world()
+  #  end
 
 end
